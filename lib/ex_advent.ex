95288d853feabd378f128032112981107380539a
Daniel Couper <danielcouper81@googlemail.com>
2018-12-03 00:49:08 +0000

    Day 1 solution

--- lib/ex_advent.ex
+++ lib/ex_advent.ex
@@ -1,18 +1,2 @@
 defmodule ExAdvent do
-  @moduledoc """
-  Documentation for ExAdvent.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExAdvent.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
